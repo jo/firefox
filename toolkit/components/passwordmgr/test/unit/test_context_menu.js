8debda3719e090464e41bbe7d7cc9bc52b0575f3
Johannes Jörg Schmidt <joschmidt@mozilla.com>
2025-06-12 07:41:42 +0200

    Bug 1971674 - monitor storage-json storage and update storage-rust on change
    This also resets the storage adapter to the legacy one (storage-json)
    and resets the tests accordingly. A new test has been added for the
    mirroring.
    
    Differential Revision: https://phabricator.services.mozilla.com/D259800

--- toolkit/components/passwordmgr/test/unit/test_context_menu.js
+++ toolkit/components/passwordmgr/test/unit/test_context_menu.js
@@ -326,7 +326,7 @@ function checkLoginItems(actualItems, expectedDetails) {
       expectedLabel +=
         " (" +
         dateAndTimeFormatter.format(
-          new Date()
+          new Date(expectedDetail.login.timePasswordChanged)
         ) +
         ")";
     }
